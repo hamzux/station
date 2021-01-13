d2f914925276512a3aaea8c7808529b26b15aecf
Hamza <47524220+hamzux@users.noreply.github.com>
2021-01-13 11:36:22 +0100

    Update station_test.c

--- station_test.c
+++ station_test.c
@@ -70,13 +70,10 @@ int main ( void )
       CU_cleanup_registry();
       return CU_get_error();
    }
+  CU_automated_run_tests();
 
    // Run all tests using the basic interface
-   CU_basic_set_mode(CU_BRM_VERBOSE);
-   CU_basic_run_tests();
-   printf("\n");
-   CU_basic_show_failures(CU_get_failure_list());
-   printf("\n\n");
+  
 /*
    // Run all tests using the automated interface
    CU_automated_run_tests();
