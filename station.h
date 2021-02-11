2c562ac63e5c292f1889da42c893f4b12199d944
Hamza <47524220+hamzux@users.noreply.github.com>
2021-02-11 19:49:25 +0100

    Update station.h

--- station.h
+++ station.h
@@ -1,8 +1,1 @@
-#ifndef STATION_H
-#define STATION_H
-extern char* position(char*);
-extern float moyenne (float, float);
-extern int nb_valeurs_aberrantes (float, float, float);
-extern float controlleur(float, float, float);
-extern int detection_pic(float, float val);
-#endif
+
